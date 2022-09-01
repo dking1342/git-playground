e4acc6a3464972bad17227bceba5c84b76b32d44
Kavooce <kavooce.cheetah@gmail.com>
2022-08-31 23:45:28 -0500

    made three pages and navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import { Link, Route, Routes } from 'react-router-dom'
+import Home from './components/Home';
+import About from './components/About';
+import "../src/index.css";
+import Contact from './components/Contact';
 
 type Props = {}
 
 const App = (props: Props) => {
-  return (
-    <div>
-      <h1>Home</h1>
+  return (    
+    <div className='container'>
+      <nav>
+        <ul>
+          <li>
+            <Link to={"/"} >Home</Link>
+          </li>
+          <li>
+            <Link to={"/about"} >About</Link>
+          </li>
+          <li>
+            <Link to={"/contact"} >Contact</Link>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <button>Sign Up</button>
+          </li>
+          <li>
+            <button>Login</button>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   )
 }

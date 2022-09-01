82b8f4e8f76ca722f384bf20ee81d11f4e7c9244
Kavooce <kavooce.cheetah@gmail.com>
2022-08-31 22:34:32 -0500

    made react project

--- dist/contact.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-console.log("contact page");
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const App = (props: Props) => {
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  )
+}
+
+export default App
